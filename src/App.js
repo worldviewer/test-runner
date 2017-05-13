7007aff47db83b00d727ddf1dcedf1815674295a
Chris Reeve <paradigmsareconstructed@gmail.com>
2017-05-13 16:25:15 -0700

    Added test code to project

--- src/App.js
+++ src/App.js
@@ -2,10 +2,43 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			tests: [
+				{ description: "commas are rotated properly",          run: this.generateDummyTest() },
+				{ description: "exclamation points stand up straight", run: this.generateDummyTest() },
+				{ description: "run-on sentences don't run forever",   run: this.generateDummyTest() },
+				{ description: "question marks curl down, not up",     run: this.generateDummyTest() },
+				{ description: "semicolons are adequately waterproof", run: this.generateDummyTest() },
+				{ description: "capital letters can do yoga",          run: this.generateDummyTest() }
+			]
+		};
+	}
+
+	callback() {
+		console.log('success');
+	}
+
+	generateDummyTest() {
+		var delay = 7000 + Math.random() * 7000;
+		var testPassed = Math.random() > 0.5;
+
+		return function(callback) {
+			setTimeout(function() {
+				callback(testPassed);
+			}, delay);
+		};
+	}
+
 	render() {
+		console.log(this.state.tests);
+		this.state.tests.forEach((test) => test.run(this.callback));
+
 		return (
 			<div className="App">
-				
+
 			</div>
 		);
 	}
