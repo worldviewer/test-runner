1410ed3029ffb020a659284699b5415bca8a246f
Chris Reeve <paradigmsareconstructed@gmail.com>
2017-05-13 17:21:17 -0700

    Tests now run, now need to create the UI components

--- src/App.js
+++ src/App.js
@@ -13,36 +13,80 @@ class App extends Component {
 				{ description: "question marks curl down, not up",     run: this.generateDummyTest() },
 				{ description: "semicolons are adequately waterproof", run: this.generateDummyTest() },
 				{ description: "capital letters can do yoga",          run: this.generateDummyTest() }
-			]
+			],
+
+			testResults: [
+				"Not Started Yet",
+				"Not Started Yet",
+				"Not Started Yet",
+				"Not Started Yet",
+				"Not Started Yet",
+				"Not Started Yet"
+			],
+
+			disabled: false
 		};
 
 		this.startButtonClickHandler = this.startButtonClickHandler.bind(this);
+		this.callback = this.callback.bind(this);
 	}
 
-	callback() {
-		console.log('success');
+	callback(testPassed, testNumber) {
+		console.log(testNumber + ': ' + testPassed);
+
+		this.setState({
+			testResults: [
+				...this.state.testResults.slice(0, testNumber),
+				testPassed ? 'Passed' : 'Failed',
+				...this.state.testResults.slice(testNumber+1)
+			]
+		});
+
+		console.log(this.state);
 	}
 
-	generateDummyTest() {
+	generateDummyTest(testNumber) {
 		var delay = 7000 + Math.random() * 7000;
 		var testPassed = Math.random() > 0.5;
 
-		return function(callback) {
+		return function(callback, testNumber) {
 			setTimeout(function() {
-				callback(testPassed);
+				callback(testPassed, testNumber);
 			}, delay);
 		};
 	}
 
 	startButtonClickHandler(e) {
 		console.log(this.state.tests);
-		this.state.tests.forEach((test) => test.run(this.callback));
+		this.state.tests.forEach((test, testNumber) => test.run(this.callback, testNumber));
+
+		this.setState({
+			testResults: Array.from({length: 6}, el => 'Running'),
+			disabled: true
+		});
+
+		console.log(this.state);
+	}
+
+	enableButton() {
+		this.setState({
+			disabled: false
+		});
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (!this.state.testResults.includes('Running') && this.state.disabled) {
+			this.enableButton();			
+		}
 	}
 
 	render() {
 		return (
 			<div className="App">
-				<button id="start_button" onClick={this.startButtonClickHandler}>Start Tests</button>
+				<button id="start_button" onClick={this.startButtonClickHandler}
+					disabled={this.state.disabled}>
+					Start Tests
+				</button>
 
 
 			</div>
